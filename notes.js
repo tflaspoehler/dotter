9ceed375ffff6525ced1aa90722972069065c308
tflaspoehler <tflaspoehler@gmail.com>
2018-12-31 19:47:22 -0500

    made subheadings work

--- notes.js
+++ notes.js
@@ -14,6 +14,16 @@ var check_fills = ["",
                    "rgb(45, 65, 205)",
                    "rgb(45, 65, 205)"]
 
+var n_lines = 1
+				   
+function user_info() {
+  document.getElementById("footer").innerHTML = 'getting data';
+    $.get("user.php", function(data, status){
+        var users = $.parseJSON(data);
+        document.getElementById("footer").innerHTML = "<b>" + users[0].user_login + "</b> (" + users[0].user_first + " " + users[0].user_last + ")";
+    });
+}
+
 today =  new Date();
 y = today.getFullYear();
 m = today.getMonth() + 1;
@@ -24,7 +34,7 @@ document.getElementById("date").innerHTML = day + " (" + m + "/" + d + "/" + y +
 function check_book() {
     var w = window.outerWidth;
     var h = window.outerHeight;
-    if (w > 800) {
+    if (w < 0) {
       journal = document.getElementById("journal");
       journal.style.borderLeft = "10px solid #123";
       journal.style.borderTopLeftRadius = "20px";
@@ -56,10 +66,14 @@ function add_listeners() {
   $( ".change" ).unbind().click(bullet_change); 
   $( ".plus" ).unbind().click(add_line); 
   $( ".ex" ).unbind().click(delete_line); 
-  $( ".adder" ).unbind().click(new_line); 
+  $( ".adder" ).unbind().click(new_line);
+  $( ".descriptor" ).unbind().click(star_line);
+  $( ".lining" ).unbind().click(change_lines);
 }
 
 $(document).ready(add_listeners);
+
+$(document).ready(user_info);
 
 $("body").on('DOMSubtreeModified', ".line", add_listeners);
 
@@ -67,38 +81,81 @@ $("body").on('DOMSubtreeModified', ".line", add_listeners);
 function new_line(event) {
   event.stopPropagation();
   console.log('adding line');
-  var next = ' <div class="line"><svg id="first" class="bullet" checked="0" xmlns="http://www.w3.org/2000/svg" version="1.0"  width="50" height="20" viewBox="0 0 50 20">';
-  next += '<path d="M29 11.5 c -1.8 0, -2.4 -1.2, -2.4 -2.4 c 0 -1.8, 1.2 -2.4, 2.4 -2.4 c 1.8 0, 2.4 1.2, 2.4 2.4 c 0 1.8, -1.2 2.4, -2.4 2.4" class="dot" id="first.checkers" />';
-  next += '<path d="M 25 7 L29 11 L35 1" class="check" id="first.check" />';
-  next += '  <path d="M 40 14 C 44 10, 44 8, 40 4 m -2 4 l2 -4 l4 2" id="first.change" class="change" stroke="%23333" stroke-width="0.4" fill="transparent"/>';
-  next += '<path d="M 19 9 l -5 0 M 16.5 6.5 l 0 5" class="plus" id="first.plus" />';
-  next += '<path d="M 5 6.5 l 5 5 M 10 6.5 l -5 5" class="ex" id="first.ex" />';
-  next += '</svg>';
-  next += '<div class="text" contenteditable="true">note 2</div>';
+  n_lines += 1
+  var next = '';
+  next += '<div class="line" id="' + n_lines + '.line">';
+  next += '    <svg id="' + n_lines + '" class="bullet" checked="0" xmlns="http://www.w3.org/2000/svg" version="1.0"  viewBox="0 0 55 20" style="position: relative; top: 0; left: -1.5em; z-index: 100;">';
+  next += '      <rect x="2.5" y="0" width="10" height="20" class="ex option click_box" id="' + n_lines + '.ex.box" />';
+  next += '      <path d="M 5 6.5 l 5 5 M 10 6.5 l -5 5" class="ex option" id="' + n_lines + '.ex" />';
+  next += '      ';
+  next += '      <rect x="12.5" y="0" width="10" height="20" class="descriptor option click_box" id="' + n_lines + '.descriptor.box" fill="rgba(0, 0, 0, 0)" stroke="rgba(0, 0, 0, 0)" stroke-width="1"/>';
+  next += '      <path d="M 15 2.5 l 0.5 7 l 4 0 l 0.5 -7 l -5 0 m 2.5 12.5 c -1.8 0, -2.0 -1.0, -2.0 -2.0 c 0 -1.8, 1.0 -2.0, 2.0 -2.0 c 1.8 0, 2.0 1.0, 2.0 2.0 c 0 1.8, -1.0 2.0, -2.0 2.0" id="' + n_lines + '.descriptor" class="descriptor option" fill="#33f"/>';
+  next += '      ';
+  next += '      <rect x="24" y="0" width="10" height="20" class="dot option click_box" id="' + n_lines + '.checkers.box" fill="rgba(0, 0, 0, 0)" stroke="rgba(0, 0, 0, 0)" stroke-width="1"/>';
+  next += '      <path d="M 24 4 h10 v10 h-10 v-10" class="dot option" id="' + n_lines + '.checkers"/>';
+  next += '      <path d="M 25 7 L29 11 L35 1" class="check" id="' + n_lines + '.check" />';
+  next += '      <rect x="35.5" y="0" width="10" height="20" class="change option click_box" id="' + n_lines + '.change.box" fill="rgba(0, 0, 0, 0)" stroke="rgba(0, 0, 0, 0)" stroke-width="1"/>';
+  next += '      <path d="M 40 14 C 44 10, 44 8, 40 4 m -2 4 l2 -4 l4 2" id="' + n_lines + '.change" class="change option" stroke="#333" stroke-width="0.4" fill="transparent"/>';
+  next += '      <rect x="46.0" y="0" width="9" height="20" class="plus option click_box" id="' + n_lines + '.plus.box" />';
+  next += '      <path d="M 48.5 9 l 5 0 m -2.5 -2.5 l 0 5" class="plus option" id="' + n_lines + '.plus" />';
+  next += '    </svg>';
+  next += '  <div class="text" contenteditable="true" style="position: relative; padding-left: 0; top: 0;">&nbsp;';
+  next += '  </div>';
   next += '</div>';
+  
   event.target.insertAdjacentHTML('beforebegin', next);
+  $(document).ready(add_listeners);
 }
 function add_line(event) {
   event.stopPropagation();
   var boss = event.target.id.split(".")[0];
   line = document.getElementById(boss+".line");
+  lines = document.getElementById(boss+".line").querySelectorAll('*[id]');
+  lines = document.getElementById(boss+".line").querySelectorAll(':scope > .line');
+  var number = 1;
+  if (lines.length > 0) {
+	  number = lines[lines.length-1].id.split(".")[0].split("-");
+	  number = parseInt(number[number.length-1]) + 1;
+  }
+  for (var i = 0; i < lines.length; ++i) {
+	  console.log('these are the lines ' + lines[i].id);
+  }
   console.log('adding line');
-  var next = ' <div class="line"><svg id="first" class="bullet" checked="0" xmlns="http://www.w3.org/2000/svg" version="1.0"  width="50" height="20" viewBox="0 0 50 20">';
-  next += '<path d="M29 11.5 c -1.8 0, -2.4 -1.2, -2.4 -2.4 c 0 -1.8, 1.2 -2.4, 2.4 -2.4 c 1.8 0, 2.4 1.2, 2.4 2.4 c 0 1.8, -1.2 2.4, -2.4 2.4" class="dot" id="first.checkers" />';
-  next += '<path d="M 25 7 L29 11 L35 1" class="check" id="first.check" />';
-  next += '  <path d="M 40 14 C 44 10, 44 8, 40 4 m -2 4 l2 -4 l4 2" id="first.change" class="change" stroke="%23333" stroke-width="0.4" fill="transparent"/>';
-  next += '<path d="M 19 9 l -5 0 M 16.5 6.5 l 0 5" class="plus" id="first.plus" />';
-  next += '<path d="M 5 6.5 l 5 5 M 10 6.5 l -5 5" class="ex" id="first.ex" />';
-  next += '</svg>';
-  next += '<div class="text" contenteditable="true">note 2</div>';
+  boss += "-" + number;
+  var next = '';
+  next += '<div class="line" id="' + boss + '.line">';
+  next += '    <svg id="' + boss + '" class="bullet" checked="0" xmlns="http://www.w3.org/2000/svg" version="1.0"  viewBox="0 0 55 20" style="position: relative; top: 0; left: -1.5em; z-index: 100;">';
+  next += '      <rect x="2.5" y="0" width="10" height="20" class="ex option click_box" id="' + boss + '.ex.box" />';
+  next += '      <path d="M 5 6.5 l 5 5 M 10 6.5 l -5 5" class="ex option" id="' + boss + '.ex" />';
+  next += '      ';
+  next += '      <rect x="12.5" y="0" width="10" height="20" class="descriptor option click_box" id="' + boss + '.descriptor.box" fill="rgba(0, 0, 0, 0)" stroke="rgba(0, 0, 0, 0)" stroke-width="1"/>';
+  next += '      <path d="M 15 2.5 l 0.5 7 l 4 0 l 0.5 -7 l -5 0 m 2.5 12.5 c -1.8 0, -2.0 -1.0, -2.0 -2.0 c 0 -1.8, 1.0 -2.0, 2.0 -2.0 c 1.8 0, 2.0 1.0, 2.0 2.0 c 0 1.8, -1.0 2.0, -2.0 2.0" id="' + boss + '.descriptor" class="descriptor option" fill="#33f"/>';
+  next += '      ';
+  next += '      <rect x="24" y="0" width="10" height="20" class="dot option click_box" id="' + boss + '.checkers.box" fill="rgba(0, 0, 0, 0)" stroke="rgba(0, 0, 0, 0)" stroke-width="1"/>';
+  next += '      <path d="M 24 4 h10 v10 h-10 v-10" class="dot option" id="' + boss + '.checkers"/>';
+  next += '      <path d="M 25 7 L29 11 L35 1" class="check" id="' + boss + '.check" />';
+  next += '      <rect x="35.5" y="0" width="10" height="20" class="change option click_box" id="' + boss + '.change.box" fill="rgba(0, 0, 0, 0)" stroke="rgba(0, 0, 0, 0)" stroke-width="1"/>';
+  next += '      <path d="M 40 14 C 44 10, 44 8, 40 4 m -2 4 l2 -4 l4 2" id="' + boss + '.change" class="change option" stroke="#333" stroke-width="0.4" fill="transparent"/>';
+  next += '      <rect x="46.0" y="0" width="9" height="20" class="plus option click_box" id="' + boss + '.plus.box" />';
+  next += '      <path d="M 48.5 9 l 5 0 m -2.5 -2.5 l 0 5" class="plus option" id="' + boss + '.plus" />';
+  next += '    </svg>';
+  next += '  <div class="text" contenteditable="true" style="position: relative; padding-left: 0; top: 0;">&nbsp;';
+  next += '  </div>';
   next += '</div>';
   line.innerHTML = line.innerHTML + next;
+  $(document).ready(add_listeners);
 }
 
 function delete_line(event) {
   event.stopPropagation();
   event.target.parentNode.parentNode.remove()
   console.log('deleting line');
+}
+function change_lines(event) {
+  event.stopPropagation();
+  days = document.getElementById("days");
+  $('#days').attr('class', 'days');
+  $('#days').toggleClass(event.target.innerHTML);
 }
 
 function box_click(event) {
@@ -118,12 +175,30 @@ function box_click(event) {
   }
 }
 
+function star_line(event) {
+  event.stopPropagation();
+  var boss = event.target.id.split(".")[0]
+  check = document.getElementById(boss);
+  if (check.starred === 1) {
+    console.log('not checked')
+    check.starred = 0;
+    document.getElementById(check.id+".descriptor").style.visibility = 'hidden';
+    document.getElementById(check.id+".descriptor").style.fill = 'rgba(0, 0, 0, 0.3)';
+  }
+  else {
+    console.log('check');
+    check.starred = 1;
+    document.getElementById(check.id+".descriptor").style.visibility = 'visible';
+    document.getElementById(check.id+".descriptor").style.fill = '#33f';
+  }
+}
+
 function bullet_change(event) {
   event.stopPropagation();
   var boss = event.target.id.split(".")[0]
   box = document.getElementById(document.getElementById(boss).id+".checkers");
   check = document.getElementById(document.getElementById(boss).id+".check");
-  console.log('changing');
+  console.log('changing '+event.target.id);
   for (i = 0; i < shapes.length; i++) {
     if (box.getAttribute('d') === shapes[i]) {
       break;
@@ -135,5 +210,4 @@ function bullet_change(event) {
   box.style.fill = fills[i];
   check.setAttribute('d', checks[i]);
   check.style.fill = check_fills[i];
-
 }
